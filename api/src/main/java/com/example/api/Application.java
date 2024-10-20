9c6e72dfb30cf6b52a5bb6d0b288dc1020d66a9a
Md Ismail Hossain <shojeeb.aust@gmail.com>
2024-10-20 10:06:46 +0200

    first endpoint created

--- api/src/main/java/com/example/api/Application.java
+++ api/src/main/java/com/example/api/Application.java
@@ -1,13 +1,36 @@
 package com.example.api;
 
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.example.api.run.Location;
+import com.example.api.run.Run;
+
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5, Location.OUTDOOR);
+			log.info("Run: {}", run);
+		};	
 	}
 
 }
